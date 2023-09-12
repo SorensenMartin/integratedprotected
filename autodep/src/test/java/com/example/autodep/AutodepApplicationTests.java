60c7184d7ba52d43ebaa1f9b92cfd15116815c91
EC2 Default User <ec2-user@ip-172-31-46-62.eu-west-1.compute.internal>
2023-09-12 09:16:45 +0000

    springboot

---
+++ autodep/src/test/java/com/example/autodep/AutodepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autodep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
