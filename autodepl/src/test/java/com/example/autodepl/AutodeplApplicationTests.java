616a7f6fc4a87ac3b1daeb00016430c8bb057140
EC2 Default User <ec2-user@ip-172-31-46-62.eu-west-1.compute.internal>
2023-09-12 09:37:19 +0000

    maven support

---
+++ autodepl/src/test/java/com/example/autodepl/AutodeplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autodepl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutodeplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
